efeb20417db6534f0871a464ec5df29b1302366c
AwanishVinayak <vinayakboy99@gmail.com>
2026-03-03 03:51:42 +0530

    Add Meta Pixel tracking snippet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,21 @@ export default function RootLayout({
             })(window,document,'script','dataLayer','GTM-M4RXD6R8');
           `}
         </Script>
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '755344377398744');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${playfair.variable} antialiased min-h-screen flex flex-col`}
@@ -54,6 +69,16 @@ export default function RootLayout({
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* Meta Pixel Code (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=755344377398744&ev=PageView&noscript=1"
+            alt=""
           />
         </noscript>
         <VisitorTracker />
